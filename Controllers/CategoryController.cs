deed5a7dd53c31ebb69d9a1a4a8dd255546a8849
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-01-03 00:16:54 -0300

    try

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -30,44 +30,78 @@ public class CategoryController : ControllerBase
     [HttpPost("v1/categories")]
     public async Task<IActionResult> PostAsync([FromBody] Category model, [FromServices] BlogDataContext context)
     {
-        await context.Categories.AddAsync(model);
-        await context.SaveChangesAsync();
+        try
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
 
-        return Created($"v1/categories/{model.Id}", model);
+            return Created($"v1/categories/{model.Id}", model);
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return StatusCode(500,"05XE9 - Não foi possível imcuir a categoria");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05XE9 - Falha interna no servido");
+        }
     }
 
     [HttpPut("v1/categories/{id:int}")]
     public async Task<IActionResult> PutAsync([FromBody] Category model, [FromRoute] int id, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-        return Ok(category);
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(category);
 
-        if (category == null)
-            return NotFound();
+            if (category == null)
+                return NotFound();
 
-        category.Name = category.Name;
-        category.Slug = category.Slug;
+            category.Name = category.Name;
+            category.Slug = category.Slug;
 
-        context.Categories.Update(category);
-        await context.SaveChangesAsync();
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
 
-        return Ok(model);
+            return Ok(model);
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return StatusCode(500, "05XE8 - Não foi possível alterar a categoria");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05XE11 - Falha interna no servido");
+        }
     }
 
 
     [HttpDelete("v1/categories/{id:int}")]
     public async Task<IActionResult> DeleteAsync([FromRoute] int id, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-        return Ok(category);
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(category);
 
-        if (category == null)
-            return NotFound();
+            if (category == null)
+                return NotFound();
 
-        context.Categories.Remove(category);
-        await context.SaveChangesAsync();
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
 
-        return Ok(category);
+            return Ok(category);
+        }
+
+        catch (DbUpdateConcurrencyException ex)
+        {
+            return StatusCode(500, "05XE7 - Não foi possível excluir a categoria");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05XE12 - Falha interna no servido");
+        }
     }
 
 
