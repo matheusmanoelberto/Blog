918317c24fe4153f461ffa5097455c3ecf00f49d
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-01-10 18:12:47 -0300

    Criando token

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    //TOKEN - JWT - Json Web Token
+    public static string JwtKey { get; set; } = "51f07a62458d4572b40cecef67083e90=";
+}
---
+++ Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+public class AccountController : ControllerBase
+{
+    [HttpPost("v1/login")]
+    public IActionResult Login([FromServices] TokenService tokenService)
+    {
+        var token = tokenService.GenerateToken(null);
+
+        return Ok(token);
+    }
+}
