3de918ba57f8ceee1aea50edfedc2d617fa78a7e
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-01-19 00:00:07 -0300

    Autenticando

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Models;
+﻿using Blog.Extensions;
+using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,15 +13,10 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClains();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.Name, value:"matheusmanoel"), //User,Identity.Name
-                new Claim(ClaimTypes.Role, value:"user"), //User.IsInRole
-                new Claim(ClaimTypes.Role, value:"admin"), //User.IsInRole
-                new Claim("fruta", value:"maça")
-            }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature ),
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "O E-mail é Obrigatório")]
+    [EmailAddress(ErrorMessage ="O E-mail é Inválido")]
+    public string Email { get; set; }
+
+
+    [Required(ErrorMessage = "Informe a senha")]
+    public string Password { get; set; }
+}
