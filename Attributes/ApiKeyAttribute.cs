2a79a2cfba184745f3f905ad6dae4de3ea0570aa
matheusmanoelberto <matheusmanoel51@gmail.com>
2024-01-22 23:13:25 -0300

    api key attribute

---
+++ Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Blog.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+       if (!context.HttpContext.Request.Query.TryGetValue(Configuration.apiKeyname, out var extractedApiKey))
+       {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 401,
+                Content = "ApiKey n~ao encontrada"
+            };
+            return;
+       }
+
+       if(!Configuration.apiKey.Equals(extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 403,
+                Content = "acesso não autorizado"
+            };
+            return;
+        }
+
+       await next();
+    }
+}
--- Configuration.cs
+++ Configuration.cs
@@ -3,5 +3,7 @@
 public static class Configuration
 {
     //TOKEN - JWT - Json Web Token
-    public static string JwtKey { get; set; } = "51f07a62458d4572b40cecef67083e90=";
+    public static string JwtKey = "51f07a62458d4572b40cecef67083e90=";
+    public static string apiKeyname = "api_key";
+    public static string apiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
 }
